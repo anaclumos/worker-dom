984d807dbf44b0d39f60ca2dace67594625d7dfb
Sunghyun Cho <mail@chosunghyun.com>
2022-08-03 17:04:17 +0900

    add sync test

--- demo/two-roots/two-roots.js
+++ demo/two-roots/two-roots.js
@@ -4,4 +4,5 @@ btn.addEventListener('click', () => {
   const infoEl = document.getElementById('info');
   const boundingClientRect = infoEl.getBoundingClientRect();
   infoEl.textContent = JSON.stringify(boundingClientRect);
+  infoEl.textContent += '\n this should go after the previous line';
 });
